f0b5913f3f26bee97049cddfd5be4e695dc67a70
Hubert_Michael_Seelan <hubertmichaelseelan@gmail.com>
2024-03-08 14:19:37 +0530

    updated

--- src/Pages/OrderConfirmed.jsx
+++ src/Pages/OrderConfirmed.jsx
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect } from 'react'
+// import React, { useContext, useEffect } from 'react'
 import "./CSS/OC.css"
-import { ShopContext } from '../Context/ShopContext';
+// import { ShopContext } from '../Context/ShopContext';
 
 const OrderConfirmed = () => {
-  const {removeFromCart} = useContext(ShopContext);
+  // const {removeFromCart} = useContext(ShopContext);
 
-  useEffect(() => {
-    removeFromCart([])
-  },[])
+  // useEffect(() => {
+  //   removeFromCart([])
+  // },[])
   
   return (
     <div className='OC'>
